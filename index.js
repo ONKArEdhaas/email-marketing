9a62453fd4b71e5d5a2cb604ee22b188c8963117
ONKAr156 <borgaonkar1998@gmail.com>
2024-10-15 15:19:00 +0530

    updated

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 3000;
 
 // CORS options
 const corsOptions = {
-    origin: 'http://localhost:5173' || 'https://incandescent-choux-02bcae.netlify.app/', // Update this to match your frontend URL
+    origin: 'https://incandescent-choux-02bcae.netlify.app/',
+    // origin: 'http://localhost:5173' || 'https://incandescent-choux-02bcae.netlify.app/', // Update this to match your frontend URL
     credentials: true,
     optionsSuccessStatus: 200
 };
